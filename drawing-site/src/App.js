a0f7f4801e9f3a2039631659c3d26282e2c76dbe
HiengLyhor <hieng.lyhoir6764@gmail.com>
2026-01-29 13:39:39 +0700

    Init Excalidraw in

---
+++ drawing-site/src/App.js
@@ -0,0 +1,10 @@
+import { Excalidraw } from "@excalidraw/excalidraw";
+import './App.css';
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Excalidraw />
+    </div>
+  );
+}
